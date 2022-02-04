abdd84e736b5e6360c90f6a325b6c6649a0a4e84
alexgharibi <ali52346@gmail.com>
2022-02-04 09:04:58 -0500

    Sync up use context

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const value = useContext(BlogContext);
+  console.log(value);
   return (
     <View>
       <Text>hi</Text>
+      <Text>{value}</Text>
     </View>
   );
 };
