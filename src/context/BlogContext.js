22565d15cad8e0119354a21ab2525a31c4d98c7f
alexgharibi <ali52346@gmail.com>
2022-02-05 08:34:56 -0500

    Add reducer to the app

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={6}>{children}</BlogContext.Provider>;
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+  const addBlogPost = () => {
+    dispatch({ type: "add_blogpost" });
+  };
+
+  const value = {
+    data: blogPosts,
+    addBlogPost,
+  };
+
+  return <BlogContext.Provider value={value}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
