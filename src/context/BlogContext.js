9f9ac1f1197d5e8bb3984f0e7f6333c2e55f4abf
alexgharibi <ali52346@gmail.com>
2022-02-12 12:04:34 -0500

    finished the fetch functions

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,15 +7,6 @@ const blogReducer = (state, action) => {
       return action.payload;
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "edit_blogpost":
       return state.map((blogPost) => {
         if (blogPost.id === action.payload.id) {
@@ -38,11 +29,8 @@ const getBlogPost = (dispatch) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({
-      type: "add_blogpost",
-      payload: { title: title, content: content },
-    });
+  return async (title, content, callback) => {
+    await jsonServer.post("/blogposts", { title, content });
     if (callback) {
       callback();
     }
@@ -50,13 +38,15 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({ type: "edit_blogpost", payload: { id, title, content } });
     if (callback) {
       callback();
